297c6bc7f48567fd106fb0ab9cbaa36e9dfc5a77
drumev <drumev@MacBook-Pro-de-Milan.local>
2014-06-09 17:53:19 +0300

    More game logic
    Planes can move, so can the bullets. Update method for the bullets,
    it’s removing all the bullets that have hit a plane or have left the
    game field

--- game-engine.js
+++ game-engine.js
@@ -1,39 +1,68 @@
-document.body.addEventListener("keydown", function (e) {
-    if (!e) {
-        e = window.event;
+/*global Kinetic, GameObject */
+/*jslint plusplus: true */
+/*jslint browser:true */
+var GameEngine = (function () {
+    'use strict';
+    var bullets = [],
+        i,
+    // FOR testing only, will be fixed after
+        plane = new GameObject.MovingObject(100, 100, 'model', 1);
+
+    function updatePlanePosition(plane, direction) {
+        plane.move(direction);
     }
-    switch (e.keyCode) {
-        //Space -> pausing the game
-        case 32:
-            isPaused = !isPaused;
-            break;
-        // left
-        case 37:
-            if (directionX === 0) {
-                directionY = 0;
-                directionX = -10;
+
+    document.body.addEventListener("keydown", function (e) {
+        if (!e) {
+            e = window.event;
+        }
+        switch (e.keyCode) {
+            //Space -> pausing the game
+            case 32:
+                //isPaused = !isPaused;
+                // left
+                break;
+            case 37:
+                updatePlanePosition(player, 'left')
+                break;
+            // up
+            case 38:
+                updatePlanePosition(player, 'up')
+                break;
+            // right
+            case 39:
+                updatePlanePosition(player, 'right')
+                break;
+            // down
+            case 40:
+                updatePlanePosition(player, 'down')
+                break;
+        }
+    });
+
+    function updateBullets() {
+        var bulletListLength = bullets.length,
+            currentBullet;
+
+        for (i = 0; i < bulletListLength; i++) {
+            currentBullet = arguments[i];
+            // Test if the bullet is inside the canvas
+            if (currentBullet.y < -currentBullet.model.height || currentBullet.y > canvas.height || currentBullet.hasHitAPlane) {
+                i--;
+                bullets.splice(i, 1);
+                bulletListLength = bullets.length;
+            } else {
+                // If the bullet is inside we are changing it's position
+                if (currentBullet.direction === 'up') {
+                    currentBullet.y -= 1;
+                } else if (currentBullet.direction === 'down') {
+                    currentBullet.y += 1;
+                } else {
+                    throw new Error('Problem with the bullet direction');
+                }
             }
-            break;
-        // up
-        case 38:
-            if (directionY === 0) {
-                directionY -= 10;
-                directionX = 0;
-            }
-            break;
-        // right
-        case 39:
-            if (directionX === 0) {
-                directionY = 0;
-                directionX = 10;
-            }
-            break;
-        // down
-        case 40:
-            if (directionY === 0) {
-                directionY += 10;
-                directionX = 0;
-            }
-            break;
+        }
     }
-});
+
+
+}());
--- game-object.js
+++ game-object.js
@@ -1,8 +1,13 @@
 var GameObject = (function () {
     'use strict';
     var planeWidth = 20,
+    // when moving, the direction will change 1 pixel at the time
+    //GIT SETTINGS TEST
+        PLAYER_DIRECTION_DELTA = 1,
+    // Enumeration with the different bulletTypes
         BulletTypes = {
-            bulletType1: {model: 'model1.png', speed: 10, damage: 15}
+            classic: {model: 'classic.png', speed: 10, damage: 15},
+            advanced: {model: 'classic.png', speed: 10, damage: 15}
         };
 
     function GameObject(x, y, model) {
@@ -12,6 +17,8 @@ var GameObject = (function () {
         this.model = model;
     }
 
+    //TODO MovingObject is using Bullet and vice versa
+
     function MovingObject(x, y, model, speed) {
         GameObject.call(this, x, y, model);
         this.speed = speed;
@@ -20,11 +27,25 @@ var GameObject = (function () {
             var currentBullet = BulletTypes.bulletType;
             return new Bullet(this.x + planeWidth / 2, this.y + 10, currentBullet); //TODO DELTA Y FOR THE BULLET
         };
+
+        this.move = function (moveDirection) {
+            if (moveDirection === 'left') {
+                this.x -= PLAYER_DIRECTION_DELTA;
+            } else if (moveDirection === 'right') {
+                this.x += PLAYER_DIRECTION_DELTA;
+            } else if (moveDirection === 'up') {
+                this.y -= PLAYER_DIRECTION_DELTA;
+            } else {
+                this.y += PLAYER_DIRECTION_DELTA;
+            }
+        };
     }
 
-    function Bullet(x, y, bulletType) {
+    function Bullet(x, y, bulletType, direction) {
         MovingObject.call(this, x, y, bulletType.model, bulletType.speed);
         this.damage = bulletType.damage;
+        this.direction = direction;
+        this.hasHitAPlane = false;
     }
 
     function test() {
