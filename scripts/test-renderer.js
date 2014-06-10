7eb090e6042c5486c7d13f18779e72973e51381b
vassilpk <vassilpk@yahoo.com>
2014-06-10 11:48:09 +0300

    little changes in key commands

---
+++ scripts/test-renderer.js
@@ -0,0 +1,19 @@
+﻿var canvas = document.getElementById('canvas'),
+	ctx = canvas.getContext('2d'),
+	ctxWidth = canvas.width,
+	ctxHeight = canvas.height,
+	playerAircraft = //to add;
+
+function draw(gameObject) {
+    img = new Image();
+    img.src = gameObject.module;
+    ctx.clearRect(0, 0, ctxWidth, ctxHeight);
+    ctx.drawImage(img, 0, 0);
+}
+
+function drawFrame() {
+    draw(playerAircraft);
+    window.requestAnimationFrame(drawFrame);
+}
+
+drawFrame();
